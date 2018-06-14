d9a55ee884a4f082c5b8deb1fc1e122820eebf7a
Gabriel Valle <gvalle91@gmail.com>
2018-06-14 19:40:18 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -5,7 +5,7 @@ def my_find(collection)
   counter = 0
   while counter < collection.length
     if yield(collection[counter])
-      collection_array << collection[counter]
+      return collection[counter]
     end
     counter += 1
   end
