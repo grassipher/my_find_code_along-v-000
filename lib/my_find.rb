f26e0b3c5ffd41acacbb01f8bb87692c70313d4d
Gabriel Valle <gvalle91@gmail.com>
2018-06-14 19:38:19 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -8,6 +8,6 @@ def my_find(collection)
     collection_array << yield(collection[counter]) if yield(collection[counter])
     counter += 1
   end
-  return collection_array
+  return collection_array[0]
 
 end
